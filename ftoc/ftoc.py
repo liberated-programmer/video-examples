8c879e2608eb1112ab09dccb8f25c43cd4b00d5f
liberated-programmer <liberatedprogrammer@gmail.com>
2022-08-21 21:03:46 -0600

    Add Python ftoc

---
+++ ftoc/ftoc.py
@@ -0,0 +1,5 @@
+tempF = float(input("Enter a temperature in degrees Fahrenheit: "))
+
+# °C = (°F - 32) * 5 / 9
+tempC = (tempF - 32) * 5 / 9
+print(f"The temperature in degrees Celsius is: {tempC}")
